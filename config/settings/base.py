522aa72cc702d406d304d15028415d972a4f876d
Freston5 <carlostejedoresquina1@gmail.com>
2017-03-23 05:38:22 -0400

    Added a ProcfileC.

--- config/settings/base.py
+++ config/settings/base.py
@@ -66,7 +66,7 @@ THIRD_PARTY_APPS = [
 # Apps specific for this project go here.
 LOCAL_APPS = [
     # custom users app
-    'daysiweb.users.apps.UsersConfig',
+    #'daysiweb.users.apps.UsersConfig',
     # Your stuff: custom apps go here
     'blog',
     'photologue',
