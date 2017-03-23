290e2434994d460d88807b6fdb03abf4ca7ca1a7
Freston5 <carlostejedoresquina1@gmail.com>
2017-03-23 06:04:16 -0400

    Added a ProcfileE.

--- config/settings/base.py
+++ config/settings/base.py
@@ -276,7 +276,7 @@ SOCIALACCOUNT_ADAPTER = 'daysiweb.users.adapters.SocialAccountAdapter'
 
 # Custom user app defaults
 # Select the correct user model
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'auth.User'
 #LOGIN_REDIRECT_URL = 'users:redirect'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'account_login'
