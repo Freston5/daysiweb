66eb0a8824a3ed64eba02f781d5fd762d070ec94
Freston5 <carlostejedoresquina1@gmail.com>
2017-03-23 09:00:15 -0400

    additional files and changes for HerokuH

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,6 +72,7 @@ LOCAL_APPS = [
     'photologue',
     'sortedm2m',
     'contact_form',
+    'users',
 
 ]
 
@@ -275,7 +276,7 @@ SOCIALACCOUNT_ADAPTER = 'daysiweb.users.adapters.SocialAccountAdapter'
 
 # Custom user app defaults
 # Select the correct user model
-AUTH_USER_MODEL = 'daysiweb.users.User'
+AUTH_USER_MODEL = 'users.User'
 #LOGIN_REDIRECT_URL = 'users:redirect'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'account_login'
