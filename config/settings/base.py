ef81c5db2a0cdc00a4ad056047c6f5e70c40dc01
Freston5 <carlostejedoresquina1@gmail.com>
2017-03-23 08:53:57 -0400

    additional files and changes for HerokuE

--- config/settings/base.py
+++ config/settings/base.py
@@ -275,7 +275,7 @@ SOCIALACCOUNT_ADAPTER = 'daysiweb.users.adapters.SocialAccountAdapter'
 
 # Custom user app defaults
 # Select the correct user model
-AUTH_USER_MODEL = 'blog.Post'
+AUTH_USER_MODEL = 'daysiweb.users.User'
 #LOGIN_REDIRECT_URL = 'users:redirect'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'account_login'
